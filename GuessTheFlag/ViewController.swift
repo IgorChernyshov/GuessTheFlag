716e95cadc4747eeb8015a6f71b87f52782a18a0
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-06-19 18:06:07 +0300

    Completed project

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
 	// MARK: - Properties
 	var score = 0
 	var correctAnswer = 0
+	var questionsAnswered = 0
 	var countries: [String] = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
 
 	// MARK: - Lifecycle
@@ -36,24 +37,37 @@ class ViewController: UIViewController {
 		button2.setImage(UIImage(named: countries[1]), for: .normal)
 		button3.setImage(UIImage(named: countries[2]), for: .normal)
 		correctAnswer = Int.random(in: 0...2)
-		title = countries[correctAnswer].uppercased()
+		title = "\(countries[correctAnswer].uppercased()). Score \(score)"
 	}
 
 	// MARK: - Actions
 	@IBAction func buttonTapped(_ sender: UIButton) {
 		var title: String
-
+		var message: String?
 		if sender.tag == correctAnswer {
 			title = "Correct!"
+			message = nil
 			score += 1
 		} else {
 			title = "Wrong!"
+			message = "That's \(countries[sender.tag].uppercased())"
 			score -= 1
 		}
 
-		let alertController = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-		alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-		present(alertController, animated: true)
+		questionsAnswered += 1
+		if questionsAnswered >= 5 {
+			let alertController = UIAlertController(title: "Game Over", message: "You scored \(score) of \(questionsAnswered)", preferredStyle: .alert)
+			alertController.addAction(UIAlertAction(title: "Reset", style: .default) { _ in
+				self.score = 0
+				self.questionsAnswered = 0
+				self.askQuestion(action: nil)
+			})
+			present(alertController, animated: true)
+		} else {
+			let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+			alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+			present(alertController, animated: true)
+		}
 	}
 }
 
