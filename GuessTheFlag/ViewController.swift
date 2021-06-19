be5b15320111ec4126936b0ff6a72ff816002b86
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-06-19 13:21:21 +0300

    Day 1

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	// MARK: - Outlets
+	@IBOutlet var button1: UIButton!
+	@IBOutlet var button2: UIButton!
+	@IBOutlet var button3: UIButton!
+
+	// MARK: - Properties
+	var countries: [String] = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+	var score = 0
+
+	// MARK: - Lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		[button1, button2, button3].forEach {
+			$0?.layer.borderWidth = 1
+			$0?.layer.borderColor = UIColor.lightGray.cgColor
+		}
+		askQuestion()
 	}
 
-
+	func askQuestion() {
+		button1.setImage(UIImage(named: countries[0]), for: .normal)
+		button2.setImage(UIImage(named: countries[1]), for: .normal)
+		button3.setImage(UIImage(named: countries[2]), for: .normal)
+	}
 }
 
